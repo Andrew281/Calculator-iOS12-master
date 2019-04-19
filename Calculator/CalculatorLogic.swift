3af6aece76abe10f3886d8d794e988393bfb3a22
Андрей Чеканников <andrew281@rambler.ru>
2019-04-19 17:58:55 +0300

    Implement MVC2.

---
+++ Calculator/CalculatorLogic.swift
@@ -0,0 +1,38 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Андрей Чеканников on 19/04/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+   
+            if symbol == "+/-" {
+                return number * -1
+            }
+                
+            else if symbol == "%" {
+                return number * 0.01
+            }
+                
+            else if symbol == "AC" {
+                return 0
+            
+        }
+        
+        return nil
+        
+    }
+    
+}
